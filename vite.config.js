511c17178d2047464dbb19f97efdeb85bb511dea
عبدالمنان <msmannan00@protonmail.com>
2024-08-24 01:16:58 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,40 +9,60 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
       manifest: {
-        name: 'My PWA App',
-        short_name: 'PWA App',
-        description: 'My awesome Progressive Web App!',
-        theme_color: '#ffffff',
-        icons: [
+        "name": "Three of Cups",
+        "short_name": "3OC",
+        "start_url": "/",
+        "display": "fullscreen",
+        "background_color": "#ffffff",
+        "lang": "en",
+        "scope": "/",
+        "description": "3oC intends to become a global digital network and ecosystem fueled by our core values. A global digital trust system for people to use before they meet in person with someone they have met online. The more types of users and types of reasons, the better the ecosystem will work for everyone.",
+        "theme_color": "#ffffff",
+        "icons": [
           {
-            src: '/assets/icons/192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            "src": "/assets/icons/192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
           },
           {
-            src: '/assets/icons/512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            "src": "/assets/icons/512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
           },
           {
-            src: '/assets/icons/192x192-maskable.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable',
+            "src": "/assets/icons/192x192-maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
           },
           {
-            src: '/assets/icons/512x512-maskable.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
+            "src": "/assets/icons/512x512-maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
           },
           {
-            src: '/assets/icons/512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any',
+            "src": "/assets/icons/512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
           }
-        ]
+        ],
+        "id": "com.app.threeofcup",
+        "dir": "ltr",
+        "orientation": "portrait",
+        "categories": [
+          "social"
+        ],
+        "share_target": {
+          "method": "GET",
+          "action": "https://twitter.com/3oC_world",
+          "params": {
+            "title": "Three of Cups",
+            "url": "https://twitter.com/3oC_world",
+            "text": "Three of Cups"
+          }
+        }
       }
     })
   ]
