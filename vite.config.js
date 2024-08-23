6482ffbebef6a2f0f8907578f789c4402449a920
عبدالمنان <msmannan00@protonmail.com>
2024-08-23 23:18:31 +0500

    ccc

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,31 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
+// Vite configuration
 export default defineConfig({
+  base: "/",
+  build: {
+    sourcemap: true,
+    assetsDir: "code",
+    target: ["esnext"],
+    cssMinify: true,
+    lib: false
+  },
   plugins: [
-    react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'My PWA App',
-        short_name: 'PWA App',
-        description: 'My awesome Progressive Web App!',
-        theme_color: '#ffffff',
-        icons: [
-        {
-          src: '/assets/icons/192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: '/assets/icons/512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-        {
-          src: '/assets/icons/512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any maskable',
-        }
-        ]
+      strategies: "injectManifest",
+      injectManifest: {
+        swSrc: 'public/sw.js',
+        swDest: 'dist/sw.js',
+        globDirectory: 'dist',
+        globPatterns: [
+          '**/*.{html,js,css,json,png}',
+        ],
+      },
+      injectRegister: false,
+      manifest: false,
+      devOptions: {
+        enabled: true
       }
     })
   ]
