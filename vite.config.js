509bbab55c16e700061d8deabe935699ef81fe28
عبدالمنان <msmannan00@protonmail.com>
2024-09-07 00:36:51 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,33 +18,23 @@ export default defineConfig({
         "scope": "/",
         "description": "3oC intends to become a global digital network and ecosystem fueled by our core values. A global digital trust system for people to use before they meet in person with someone they have met online. The more types of users and types of reasons, the better the ecosystem will work for everyone.",
         "theme_color": "#ffffff",
-
-
-        "related_applications": [],
-        "prefer_related_applications": false,
-        "display_override": ["window-controls-overlay"],
-        "launch_handler": {
-          "client_mode": "focus-existing"
-        },
-
-        "icons": [
+        "id": "com.app.threeofcup",
+        "dir": "ltr",
+        "orientation": "portrait",
+        "categories": [
+          "social"
+        ],
+        "iarc_rating_id": "18",
+      },
+        "shortcuts": [
           {
-            "src": "/assets/icons/192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
-          },
-          {
-            "src": "/assets/icons/512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
-          },
-          {
-            "src": "/assets/icons/192x192-maskable.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "maskable"
+            "name": "Open About",
+            "short_name": "About",
+            "description": "Open the about page",
+            "url": "/about",
+            "icons": [{ "src": "assets/icons/192x192.png", "sizes": "192x192" }]
           }
-        },
+        ],
     })
   ]
 });
